1681b261d1dc09f0064398e37aac1a89bcd708fe
dakshshah05 <dakshshah215@gmail.com>
2025-03-01 22:45:24 +0530

    Fixed path issue

--- js/population.js
+++ js/population.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const state = document.body.getAttribute('data-state');
-    fetch('https://run.mocky.io/v3/ed10ec6d-556c-4319-809d-aa52ec29faa8')
+    fetch('https://run.mocky.io/v3/c071ca7e-b26c-49ef-9e26-aada76a95cee')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
